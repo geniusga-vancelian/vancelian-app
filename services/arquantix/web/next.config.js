93a836f387f55e2c56fadf622d52932a10d3337e
Gael Itier <gael.itier@vancelian.com>
2026-01-03 12:33:31 +0400

    Revert to next start instead of standalone (fix server not responding)

--- services/arquantix/web/next.config.js
+++ services/arquantix/web/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // output: 'standalone', // Désactivé pour utiliser next start
 }
 
 module.exports = nextConfig
