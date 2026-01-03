306557001d8f36e87f6b329bbcf8ec68309b433d
Gael Itier <gael.itier@vancelian.com>
2026-01-03 11:16:49 +0400

    Move Arquantix coming soon page from /fr to root /

--- services/arquantix/web/src/app/page.tsx
+++ services/arquantix/web/src/app/page.tsx
@@ -1,6 +1,22 @@
-import { redirect } from 'next/navigation'
+'use client'
+
+import Navbar from '@/components/arquantix/Navbar'
+import Hero from '@/components/arquantix/Hero'
+import Footer from '@/components/arquantix/Footer'
 
 export default function HomePage() {
-  redirect('/fr')
+  return (
+    <div className="min-h-screen bg-neutral-black text-white">
+      <Navbar />
+      <div className="pt-20">
+        <Hero
+          images={['/hero.jpg', '/hero-2.jpg']}
+          autoplay={true}
+          intervalMs={4500}
+        />
+      </div>
+      <Footer />
+    </div>
+  )
 }
 
